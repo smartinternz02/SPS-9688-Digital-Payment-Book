ccaf9ce43476747909479547aa3375804ff91df0
girevasan <vasan.gire@gmail.com>
2021-04-20 20:25:45 +0530

    first commit

---
+++ src/main/java/com/paymentbook/demo/GireDigitalPaymentBookApplication.java
@@ -0,0 +1,13 @@
+package com.paymentbook.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GireDigitalPaymentBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GireDigitalPaymentBookApplication.class, args);
+	}
+
+}
