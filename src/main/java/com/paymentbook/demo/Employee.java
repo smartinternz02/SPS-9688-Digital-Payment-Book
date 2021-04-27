23721e214d947c0a3c316b6a28a333f9a81ec44f
girevasan <vasan.gire@gmail.com>
2021-04-27 10:22:39 +0530

    seventh commit

--- src/main/java/com/paymentbook/demo/Employee.java
+++ src/main/java/com/paymentbook/demo/Employee.java
@@ -4,12 +4,15 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Employee {
 
-	private @Id @GeneratedValue Long id;
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String firstName;
 	private String lastName;
 	private String description;
